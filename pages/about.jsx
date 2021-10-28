edb393bc984c481a407372c6faa7be29535317e8
Manshur Notebook <manshurmuhammad2@gmail.com>
2021-10-28 22:23:22 +0700

    First Modification

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/Link';
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/"><a>Home</a></Link>
+    </>
+  );
+}
+
+export default About;
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/Link';
+
+function Home() {
+  return (
+    <div>
+      Test Page Saya
+      <Link href="/about"><a>About</a></Link>
+    </div>    
+  )
+}
+
+export default Home;
